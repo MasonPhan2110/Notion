316e33a2c63ba33783c6da972775285cd3601fd4
Mason Phan <phannhatminh1099@gmail.com>
2023-11-07 16:52:24 +0700

    add sidebar

---
+++ app/(main)/(routes)/documents/page.tsx
@@ -0,0 +1,9 @@
+const DocumentsPage = () => {
+    return ( 
+        <div>
+            This is a protected DocumentPage
+        </div>
+     );
+}
+ 
+export default DocumentsPage;
---
+++ convex/auth.config.js
@@ -0,0 +1,8 @@
+export default {
+    providers: [
+        {
+            domain: "https://funny-badger-9.clerk.accounts.dev",
+            applicationID: "convex",
+        },
+    ]
+};
