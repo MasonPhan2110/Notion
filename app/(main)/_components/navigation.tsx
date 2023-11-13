f1b37dfbe3ce3f9867f8d0769b817310283f41a5
minhpn33 <minhpn33@fpt.com>
2023-11-13 17:32:06 +0700

    add search function

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -13,9 +13,11 @@ import { toast } from "sonner";
 import { DocumentList } from "./document-list";
 import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
 import { TrashBox } from "./trash-box";
+import { useSearch } from "@/hooks/use-search";
 
 
 export const Navigation = () => {
+    const search = useSearch();
     const pathname = usePathname();
     const isMobile = useMediaQuery("(max-width:768px)");
 
@@ -138,7 +140,7 @@ export const Navigation = () => {
                      label="Search"
                      icon={Search}
                      isSearch
-                     onClick={()=>{}}
+                     onClick={search.onOpen}
                     />
                     <Item
                      label="Settings"
